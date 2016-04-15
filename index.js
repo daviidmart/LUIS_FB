1660391c8ae4003a6e81dd7e6dec87b312f8cc72
David Martinez <david.mtlopez@gmail.com>
2016-04-15 00:30:38 -0500

    V 0.0.1

---
+++ index.js
@@ -0,0 +1,32 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('FUNCIONA!');
+});
+
+app.get('/facebook', function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen();
