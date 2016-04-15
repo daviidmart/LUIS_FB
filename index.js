041460367369745dd4ecd5c910b50a919ea80b9d
David Martinez <david.mtlopez@gmail.com>
2016-04-15 00:37:44 -0500

    V 0.0.1

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('FUNCIONA!');
+  res.send('It works!');
 });
 
-app.get('/facebook', function(req, res) {
+app.get(['/facebook', '/instagram'], function(req, res) {
   if (
     req.param('hub.mode') == 'subscribe' &&
     req.param('hub.verify_token') == 'token'
@@ -26,6 +26,14 @@ app.get('/facebook', function(req, res) {
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:');
   console.log(req.body);
+  // Process the Facebook updates here
+  res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
   res.sendStatus(200);
 });
 
