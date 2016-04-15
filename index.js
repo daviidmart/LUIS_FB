d886aeadc9921eb5b5218440099379ea565bedbc
David Martinez <david.mtlopez@gmail.com>
2016-04-15 00:51:00 -0500

    V 0.0.1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
 
 app.use(bodyParser.json());
 
@@ -34,6 +45,4 @@ app.post('/instagram', function(req, res) {
   res.sendStatus(200);
 });
 
-app.listen(80, function () {
-    console.log('LUIS.FB iniciado en puerto: ' + 80);
-});
+app.listen();
