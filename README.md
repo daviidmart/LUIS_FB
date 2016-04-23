513312833fd52a830f75fa0a4dc2388a940c29a9
David Martinez <david.mtlopez@gmail.com>
2016-04-23 10:01:16 -0500

    V 0.1.30

---
+++ README.md
@@ -0,0 +1,4 @@
+# LUIS_FB
+
+### Description
+Understand language contextually, so your app communicates with people in the way they speak.
---
+++ test/example.js
@@ -0,0 +1,13 @@
+//var iris = require('iris');                                     // LIBRERIA IRIS
+//
+//var serverIris = new iris({
+//    pSock: 1812,                                                //PUERTO SOCKET.IO
+//    pApi: 1811,                                                 //PUERTO API REST
+//    aTels: ["5214491259348"],                                   //TELEFONOS A MANDAR ALERTA
+//    tEntries: '',                                               //TOKEN LOG ENRIES
+//    hRedis: '',                                                 //HOST REDIS
+//    pRedis: 6379,                                               //PUERTO REDIS
+//    tRedis: '',                                                 //TOKEN REDIS
+//    cRedis: 'Iris:Mensajes',                                    //CLAVE OBJ REDIS
+//    bRedis: 1,                                                  //NUMERO BD REDIS
+//});
